6374c6330d1c4ca0f616e3b34f2bea058d9bbef8
Arly <mrsmileball@gmail.com>
2019-01-15 23:09:54 +0200

    add comment, add example

--- calc.py
+++ calc.py
@@ -1,6 +1,5 @@
 print("Введите пример.")
 paradigm = input()
-print(paradigm)
 
 def summ_function(a,b):
 	return a + b
@@ -13,3 +12,24 @@ def subt_function(a,b):
 
 def div_function(a,b):
 	return a / b
+
+# ВАЖНО! функции объявлять перед выполнением.
+# Тут нужно будет строку,который вводит юзер разобрать на
+# числа, с которыми будут производиться вычисления. 
+# Ко всему прочему нужно определить знак и в соотношении с ним
+# запустить нужную функцию и передать параметры. И вернуть ответ.
+
+# тут предположительно будет знак и числа, которые спарсяться
+symbol = "+"
+a = 13
+b = 2
+
+switcher = {
+	'+': summ_function(a,b),
+	'-': multiple_function(a,b),
+	'*': subt_function(a,b),
+	'/': div_function(a,b)
+}
+
+
+print(switcher[symbol])
