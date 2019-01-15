e91a3f0df5eccd81d65e6a2377fe71b2bb4aad6e
Arly <mrsmileball@gmail.com>
2019-01-15 22:40:11 +0200

    start project calculator. adding four standart fucntion

---
+++ calc.py
@@ -0,0 +1,15 @@
+print("Введите пример.")
+paradigm = input()
+print(paradigm)
+
+def summ_function(a,b):
+	return a + b
+
+def multiple_function(a,b):
+	return a * b
+
+def subt_function(a,b):
+	return a - b
+
+def div_function(a,b):
+	return a / b
