b0007b60c128fc094efdc18d926540d6feb27b34
Arly0 <mrsmileball@gmail.com>
2019-01-18 10:59:30 +0200

    add sqrt function

--- calc.py
+++ calc.py
@@ -1,5 +1,6 @@
 import re
-
+#import math
+print('Калькулятор к вашим услугам. Доступные действия над числами - +, -, *, /, ^.')
 paradigm = input("Введите пример: ")
 
 def summ_function(a,b):
@@ -21,12 +22,17 @@ def div_function(a,b):
 def pow_function(a,b):
     return a ** b
 
+def sqrt_function(a,b):
+	return int(a ** (1/b))
+
 numbers = r'[0-9]+[.]?[0-9]?' #search float or int number
 allnumbers = re.findall(numbers, paradigm)
-symb = r'[\+\-\*\/\^]' #search '+-*/^'
+symb = r'[\+\-\*\/\^\&]' #search '+-*/^'
 
 paradigm_symbol = re.findall(symb, paradigm)
 
+# Для ввода 3 и больше чисел сделать присвоение цифр через цикл в другой массив и проводить
+# с ними действия
 a = float((allnumbers[0]))
 b = float((allnumbers[1]))
 symbol = (paradigm_symbol[0])
@@ -37,7 +43,7 @@ switcher = {
 	'-': subt_function(a,b),
 	'/': div_function(a,b),
     '^': pow_function(a,b),
-	'/': div_function(a,b)
+	'&': sqrt_function(a,b)
 }
 
 
