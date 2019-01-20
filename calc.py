99dd51dc69596f3db007d986702487a041446f7c
Arly0 <mrsmileball@gmail.com>
2019-01-20 17:35:31 +0200

    add many numbers

--- calc.py
+++ calc.py
@@ -1,8 +1,9 @@
 import re
-#import math
-print('Калькулятор к вашим услугам. Доступные действия над числами: +, -, *, /, ^, &(корень), $(деление с отображением остатка)')
+
+print('Калькулятор к вашим услугам. Доступные действия над числами: +, -, *, /, ^, &(корень), @(деление с отображением остатка)')
 paradigm = input("Введите пример: ")
 
+# функции выполнения задач над числами
 def summ_function(a,b):
 	return a + b
 
@@ -16,7 +17,7 @@ def div_function(a,b):
 	try:
 		a / b
 	except ZeroDivisionError:
-		return "На ноль делить нельзя"
+		return 'На ноль делить нельзя'
 	return a / b
 
 def pow_function(a,b):
@@ -25,34 +26,39 @@ def pow_function(a,b):
 def sqrt_function(a,b):
 	return int(a ** (1/b))
 
-def division_function():
+def division_function(a,b):
 	x = int(a) // int(b)
 	z = int(a) % int(b)
-	print('Ответ: ' + str(x) + '/Остаток: ' + str(z))
+	strXZ = str(x) + '| Остаток: ' + str(z)
+	return strXZ
 
+
+# разбор строки на переменные и работа с ними
 numbers = r'[0-9]+[.]?[0-9]?' #search float or int number
 allnumbers = re.findall(numbers, paradigm)
-symb = r'[\+\-\*\/\^\&\$]' #search '+-*/^&'
+symb = r'[\+\-\*\/\^\&\@]' #search '+-*/^&'
 
 paradigm_symbol = re.findall(symb, paradigm)
-
+# symbol = (paradigm_symbol[0])
 # Для ввода 3 и больше чисел сделать присвоение цифр через цикл в другой массив и проводить
 # с ними действия
-a = float((allnumbers[0]))
-b = float((allnumbers[1]))
 
-symbol = (paradigm_symbol[0])
+def result_function():
+	# выбор оператора и вызов соответствующей функции
+	switcher = {
+		'+': summ_function(a, b),
+		'*': multiple_function(a, b),
+		'-': subt_function(a, b),
+		'/': div_function(a, b),
+		'^': pow_function(a, b),
+		'&': sqrt_function(a, b),
+        '@': division_function(a,b),
+	}
+	return print('\nОтвет: ' + str(switcher[symbol]))
 
-switcher = {
-	'+': summ_function(a,b),
-	'*': multiple_function(a,b),
-	'-': subt_function(a,b),
-	'/': div_function(a,b),
-    '^': pow_function(a,b),
-	'&': sqrt_function(a,b),
-}
-
-if symbol == '$':
-	division_function()
-else:
-	print('Ответ: ' + str(switcher[symbol]))
+for i in range(len(allnumbers)):
+	symbol = (paradigm_symbol[i])
+	if int(i) % 2 == 0:
+		a = float(allnumbers[i])
+		b = float(allnumbers[i+1])
+		result_function()
